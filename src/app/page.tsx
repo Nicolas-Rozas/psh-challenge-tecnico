2ca33af0a3f7304b58607f99168e53789aea7aa7
Nicolas Rozas <rozasnicolas23@gmail.com>
2024-07-08 02:56:58 -0300

    Conectados todos los componentes y estilado de la mainPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,114 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import React, { useState, useRef, useEffect } from "react";
+import {
+  MainContainer,
+  SideNavBarContainer,
+  ChatAreaContainer,
+  InputContainer,
+  ChatContainer,
+} from "./styles";
+import Header from "../components/Header";
+import SideNavBar from "../components/SideNavBar";
+import { UserDetails } from "@/types/chatTypes";
+import ChatBubble from "../components/ChatBubble";
+import useMessage from "@/Hooks/useMessage";
+import { initialChats } from "./mockData";
+
+const Home = () => {
+  const [selectedUser, setSelectedUser] = useState<UserDetails | null>(null); // Estado para almacenar el usuario seleccionado
+  const chatEndRef = useRef<HTMLDivElement>(null); // Referencia al final del área de chat para scroll
+  const { messageText, setMessageText } = useMessage(); // Hook personalizado para manejar el texto del mensaje
+
+  // Función para manejar la selección de usuario
+  const handleSelectUser = (user: UserDetails) => {
+    setSelectedUser(user);
+  };
+
+  // Efecto para hacer scroll hacia abajo cuando se selecciona un nuevo usuario
+  useEffect(() => {
+    if (chatEndRef.current) {
+      chatEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [selectedUser]);
+
+  // Función para manejar cambios en el texto del mensaje
+  const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessageText(event.target.value);
+  };
+
+  // Función para manejar el envío de mensajes
+  const handleMessageSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!selectedUser || !messageText.trim()) {
+      return; // Evitar enviar mensajes vacíos
+    }
+
+    // Crear un nuevo mensaje
+    const newMessage = {
+      id: selectedUser.messages.length + 1,
+      text: messageText,
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      isSent: true,
+    };
+
+    // Actualizar los mensajes del usuario seleccionado
+    setSelectedUser((prevUser) => {
+      if (!prevUser) return null;
+      const updatedMessages = [...prevUser.messages, newMessage];
+      return { ...prevUser, messages: updatedMessages };
+    });
+
+    // Limpiar el campo de texto del mensaje después de enviar
+    setMessageText("");
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <MainContainer>
+      <SideNavBarContainer>
+        <SideNavBar
+          initialChats={initialChats}
+          onSelectUser={handleSelectUser}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </SideNavBarContainer>
+      {selectedUser && (
+        <ChatAreaContainer>
+          <Header
+            name={selectedUser.name}
+            role={selectedUser.role}
+            imageUrl={selectedUser.imageUrl}
+          />
+          <ChatContainer>
+            {selectedUser.messages.map((msg) => (
+              <ChatBubble
+                key={msg.id}
+                message={msg.text}
+                time={msg.time}
+                isSent={msg.isSent}
+                imageUrl={selectedUser.imageUrl}
+              />
+            ))}
+            {/* Referencia al final del área de chat para scroll */}
+            <div ref={chatEndRef} />
+          </ChatContainer>
+          <InputContainer>
+            <form onSubmit={handleMessageSubmit}>
+              <input
+                type="text"
+                value={messageText}
+                onChange={handleMessageChange}
+                placeholder="Type your message..."
+              />
+              <button type="submit">SEND</button>
+            </form>
+          </InputContainer>
+        </ChatAreaContainer>
+      )}
+    </MainContainer>
   );
-}
+};
+
+export default Home;
---
+++ src/app/styles.js
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  display: grid;
+  grid-template-columns: 30vw 1fr;
+  grid-template-rows: 100vh;
+  background-color: #e9e9e9;
+  overflow-x: hidden;
+    @media (max-width: 1024px) {
+  grid-template-columns: 40vw 1fr;
+  }
+`;
+
+export const SideNavBarContainer = styled.div`
+  grid-column: 1 / span 1;
+  grid-row: 1 / span 1;
+  overflow-y: auto;
+`;
+
+export const ChatAreaContainer = styled.div`
+  grid-column: 2 / span 1;
+  grid-row: 1 / span 1;
+  overflow-y: auto;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  overflow-x: hidden;
+
+  @media (max-width: 768px) {
+    grid-column: 1 / span 2;
+  }
+`;
+
+export const ChatContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  margin-left: 50px;
+  margin-right: 10%;
+
+  @media (max-width: 768px) {
+    margin-left: 10px;
+  }  margin-right: 5%;
+
+
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  background-color: white;
+  position: sticky;
+  bottom: 0;
+  width: 99%;
+  height: 60px;
+  overflow-x: hidden;
+  padding-left: 50px;
+
+  @media (max-width: 768px) {
+    padding-left: 10px; /
+  }
+
+  form {
+    width: 100%;
+    display: flex;
+    align-items: center;
+  }
+
+  input {
+    flex: .9;
+    border: 2px solid #f4f4f4;
+    border-radius: 12px;
+    padding: 8px 12px; 
+    margin-right: 10px;
+    height: 36px; 
+    background-color: #fbfbfb;
+    color: #777777;
+    font-size: 14px; 
+    font-weight: 600;
+
+    @media (max-width: 768px) {
+      font-size: 12px; 
+          padding: 8px 12px; 
+
+    }
+  }
+
+  button {
+    padding: 8px 16px;
+    background-color: #f0f0f0;
+    color: #7f7f7f;
+    border: none;
+    border-radius: 12px;
+    cursor: pointer;
+    font-weight: bold;
+    width: 100px;
+    height: 36px;
+
+    @media (max-width: 768px) {
+      width: 80px;
+      font-size: 12px; 
+    }
+  }
+`;
